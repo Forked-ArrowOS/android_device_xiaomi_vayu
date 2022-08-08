f31772857877fadf0c04796a53ea508f00a346dc
clhex <clhexftw@protonmail.com>
2022-08-08 09:13:52 +0530

    vayu: Drop Device Maintainer Flag
    * isnt needed in unoff builds
    
    Change-Id: Id7bcc2d044c0fd63b0ef0517ca66950e745f72e1

--- arrow_vayu.mk
+++ arrow_vayu.mk
@@ -29,6 +29,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 BUILD_FINGERPRINT := Xiaomi/vayu_global/vayu:12/RKQ1.200826.002/V13.0.3.0.SJUMIXM:user/release-keys
 
-# ArrowOS additions
-DEVICE_MAINTAINER := kubersharma001
-
